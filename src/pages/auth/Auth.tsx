a5c340199af0f73c56938de99cef0da6867fc4db
Feoktisto.v <FeoktistovSano@mail.ru>
2023-08-17 21:53:23 +0300

    fix: update relocation on '/' or '/auth'

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,28 +1,46 @@
 import { Register, Login } from "@components-auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Button, Container, Typography } from "@mui/material";
+import { token as userToken } from "@helpers";
+import { Progress } from "../../components/progress";
 
 export const Auth = () => {
     const [isLogin, setIsLogin] = useState( false );
+    const [token, setToken] = useState<boolean>( false );
+
+    useEffect( () => {
+        if ( userToken.getToken() ) {
+            setToken( true );
+        }
+    }, [userToken.getToken()] );
+
+    console.log( userToken );
 
     return (
-        <Box height="100vh" width="100%" pt={30}>
-            <Container>
-                { isLogin ? <Typography variant="h2" fontSize="20px" mb={3} textAlign="center">Вход</Typography> : <Typography variant="h2" fontSize="20px" mb={3} textAlign="center">Регистрация</Typography> }
+        <>
+            {
+                !token ? (
+                    <Box height="100vh" width="100%" pt={30}>
+                        <Container>
+                            { isLogin ? <Typography variant="h2" fontSize="20px" mb={3} textAlign="center">Вход</Typography> : <Typography variant="h2" fontSize="20px" mb={3} textAlign="center">Регистрация</Typography> }
 
-                <Box display="flex" justifyContent="center">
+                            <Box display="flex" justifyContent="center">
 
-                    <Box width="500px">
+                                <Box width="500px">
 
-                        { isLogin ? <Login /> : <Register /> }
+                                    { isLogin ? <Login /> : <Register /> }
 
-                        <Button sx={{ marginTop: "20px" }} variant="text" color="warning" onClick={() => setIsLogin( ( prev ) => !prev )}>
-                            {isLogin ? "У меня нет аккаунта" : "Уже есть аккаунт?"}
-                        </Button>
+                                    <Button sx={{ marginTop: "20px" }} variant="text" color="warning" onClick={() => setIsLogin( ( prev ) => !prev )}>
+                                        {isLogin ? "У меня нет аккаунта" : "Уже есть аккаунт?"}
+                                    </Button>
 
+                                </Box>
+                            </Box>
+                        </Container>
                     </Box>
-                </Box>
-            </Container>
-        </Box>
+                ) : <Progress />
+            }
+
+        </>
     );
 };
--- src/pages/preview/Preview.tsx
+++ src/pages/preview/Preview.tsx
@@ -1,9 +1,19 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import { useEffect, useState } from "react";
+import { token as userToken } from "@helpers";
+import { Progress } from "../../components/progress";
 
 export const Preview = () => {
     const navigate = useNavigate();
+    const [token, setToken] = useState<boolean>( false );
+
+    useEffect( () => {
+        if ( userToken.getToken() ) {
+            setToken( true );
+        }
+    }, [userToken.getToken()] );
 
     const [text] = useTypewriter( {
         words: ["обсуждать важные для вас темы.", "получать ответы на важные для вас вопросы.", "помогать другим, отвечая на вопросы."],
@@ -13,19 +23,26 @@ export const Preview = () => {
     } );
 
     return (
-        <Box sx={{ width: "100%", height: "100vh", backgroundColor: "background.default" }}>
-            <Container maxWidth="lg" sx={{ margin: "0 auto" }}>
-                <Box pt={30}>
-                    <Typography variant="h1" fontSize="40px" color="#fff" mb={3} textAlign="center">RuForum</Typography>
-                    <Typography variant="h2" fontSize="30px" color="#fff" mb={5} textAlign="center">
-                        Здесь вы можете { text }
-                        <Cursor cursorColor='red' />
-                    </Typography>
-                    <Box display="flex" justifyContent="center">
-                        <Button variant="contained" color="primary" size="large" onClick={() => navigate( "/auth" )}>Приступить</Button>
+        <>
+            {
+                !token ? (
+                    <Box sx={{ width: "100%", height: "100vh", backgroundColor: "background.default" }}>
+                        <Container maxWidth="lg" sx={{ margin: "0 auto" }}>
+                            <Box pt={30}>
+                                <Typography variant="h1" fontSize="40px" color="#fff" mb={3} textAlign="center">RuForum</Typography>
+                                <Typography variant="h2" fontSize="30px" color="#fff" mb={5} textAlign="center">
+                                    Здесь вы можете { text }
+                                    <Cursor cursorColor='red' />
+                                </Typography>
+                                <Box display="flex" justifyContent="center">
+                                    <Button variant="contained" color="primary" size="large" onClick={() => navigate( "/auth" )}>Приступить</Button>
+                                </Box>
+                            </Box>
+                        </Container>
                     </Box>
-                </Box>
-            </Container>
-        </Box>
+                ) : <Progress />
+            }
+
+        </>
     );
 };
